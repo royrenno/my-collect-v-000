2fd3445845bf9dd25eb795728633e14a073f7bab
roy renno <royrenno@yahoo.com>
2019-02-07 02:55:42 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+student = ["Tim Jones", "Tom Smith","Jim Campagno"]
+collection =["ruby","javascript","python","objective-c"]
 
-
+def my_collect(collection)
+array =[]
+i = 0
+while i < collection.length
+   array << yield(collection[i])
+   i = i+ 1
+ end
+ array
+ end
+ my_collect(collection) do |lang|
+  lang.upcase
+end
